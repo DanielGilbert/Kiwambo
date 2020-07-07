5d1c9fb407e0523e90f56c0b90f11aad5e6f0f5a
Daniel Gilbert <ich@danielgilbert.de>
2020-07-07 22:26:05 +0200

    Adds Avalonia UI

---
+++ Kiwambo.Core/Windows/WindowsMonitor.cs
@@ -0,0 +1,11 @@
+﻿using Kiwambo.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kiwambo.Core.Windows
+{
+    public class WindowsMonitor : IMonitor
+    {
+    }
+}
