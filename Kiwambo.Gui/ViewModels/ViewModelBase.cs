8855aba2a73638cf2abc33ea2d5b253f4811e3c3
Daniel Gilbert <ich@danielgilbert.de>
2020-07-07 22:26:05 +0200

    Adds Avalonia UI

---
+++ Kiwambo.Gui/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace Kiwambo.Gui.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
